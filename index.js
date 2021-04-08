805add276db6c0c8de492df52e98222467ef04f2
Augusto Lima <augustolimads@gmail.com>
2021-04-07 21:10:12 -0300

    🚧 [2/2] updating file structure

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const HtmlParser = require("./src/components/HtmlParser");
-const Processor = require("./Processor");
-const Reader = require("./Reader");
-const Table = require("./Table");
-const Writer = require("./Writer");
-const PDFWriter = require("./PDFWriter");
+const Processor = require("./src/components/Processor");
+const Reader = require("./src/components/Reader");
+const Table = require("./src/components/Table");
+const Writer = require("./src/components/Writer");
+const PDFWriter = require("./src/components/PDFWriter");
 
 async function main() {
 
   try {
     const reader = new Reader();
 
-    const data = await reader.read("./src/public/english_words.csv");
+    const data = await reader.read("src/english_words.csv");
     const processedData = Processor.Process(data);
 
     const {rowCount, columnCount, getHeaderAndRows} = new Table(processedData);
